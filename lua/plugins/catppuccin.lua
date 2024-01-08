c721ebceaee29003c4bf39f59dbacfb284c693e8
Ricardo Otero <oterosantos@gmail.com>
2024-01-08 21:27:48 +0000

    Remove the (apparently) useless extra config

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,7 +1,7 @@
 require("catppuccin").setup({
   term_colors = true,
   transparent_background = false,
-
+  flavour = "mocha",
   styles = {
     comments = {},
     conditionals = {},
@@ -33,8 +33,7 @@ require("catppuccin").setup({
     mini = true,
     cmp = true,
     telescope = {
-      enabled = false,
-      style = "minimal",
+      enabled = true,
     },
   }
 })
