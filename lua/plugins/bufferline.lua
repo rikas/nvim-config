fc6d52c387d8a802289f2aa8fb985611e75ee5b3
Ricardo Otero <oterosantos@gmail.com>
2024-01-09 17:02:04 +0000

    Tweak colors of the bufferline

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,10 +1,18 @@
 require('bufferline').setup({
   options = {
-    icon = '|', -- this should be omitted if indicator style is not 'icon'
-    style = 'icon',
+    indicator = {
+      -- icon = '▎', -- this should be omitted if indicator style is not 'icon'
+      icon = '▎', -- this should be omitted if indicator style is not 'icon'
+      style = 'icon'
+    },
     close_command = function(n) require("mini.bufremove").delete(n, false) end,
     diagnostics = "nvim_lsp",
+    diagnostics_indicator = function(count, level)
+      local icon = level:match("error") and " " or ""
+      return " " .. icon .. count
+    end,
     modified_icon = '●',
+
     close_icon = '',
     left_trunc_marker = '',
     right_trunc_marker = '',
@@ -21,3 +29,8 @@ require('bufferline').setup({
     },
   },
 })
+
+-- Change the indicator color and the selected buffer text color
+vim.api.nvim_set_hl(0, 'BufferLineIndicatorSelected', { bg = 'NONE', fg = '#bbbbee' })
+vim.api.nvim_set_hl(0, 'BufferLineBufferSelected',
+  { bg = 'NONE', fg = '#e4e4ff', bold = false, italic = false, cterm = {} })
