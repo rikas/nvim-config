d887bf04d75c078f5189a7c398ec8b9d393e38da
Ricardo Otero <oterosantos@gmail.com>
2024-01-09 18:07:18 +0000

    Simple change on the case of Esc

--- lua/rikas/cmp.lua
+++ lua/rikas/cmp.lua
@@ -28,7 +28,7 @@ cmp.setup({
     },
   },
   mapping = cmp.mapping.preset.insert {
-    ["<C-esc>"] = cmp.mapping.abort(),
+    ["<C-Esc>"] = cmp.mapping.abort(),
     ["<C-d>"] = cmp.mapping.scroll_docs(-4),
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
     ["<C-Space>"] = cmp.mapping.complete(),
