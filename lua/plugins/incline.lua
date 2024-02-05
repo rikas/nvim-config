327636b88428686ce01a23aebda21de3f37945ef
Ricardo Otero <oterosantos@gmail.com>
2024-02-05 23:26:04 +0000

    Tweak incline for a better experience
    Remove git signs as we already have then in lualine

--- lua/plugins/incline.lua
+++ lua/plugins/incline.lua
@@ -6,28 +6,11 @@ return {
       render = function(props)
         local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
         local ft_icon, ft_color = require("nvim-web-devicons").get_icon_color(filename)
-        local modified = vim.bo[props.buf].modified and "bold,italic" or "bold"
+        local is_modified = vim.bo[props.buf].modified
+        local modified = vim.bo[props.buf].modified and "bold,italic" or "none"
 
-        local function get_git_diff()
-          local icons = { removed = "-", changed = "~", added = "+" }
-          icons["changed"] = icons.modified
-          local signs = vim.b[props.buf].gitsigns_status_dict
-          local labels = {}
-          if signs == nil then
-            return labels
-          end
-          for name, icon in pairs(icons) do
-            if tonumber(signs[name]) and signs[name] > 0 then
-              table.insert(labels, { icon .. signs[name] .. " ", group = "Diff" .. name })
-            end
-          end
-          if #labels > 0 then
-            table.insert(labels, { "┊ " })
-          end
-          return labels
-        end
         local function get_diagnostic_label()
-          local icons = { error = "", warn = "", info = "", hint = "" }
+          local icons = { error = " ", warn = " ", info = " ", hint = " " }
           local label = {}
 
           for severity, icon in pairs(icons) do
@@ -47,10 +30,11 @@ return {
 
         return {
           { get_diagnostic_label() },
-          { get_git_diff() },
           { (ft_icon or "") .. " ", guifg = ft_color, guibg = "none" },
           { filename .. " ", gui = modified },
-          { "┊  " .. vim.api.nvim_win_get_number(props.win), group = "DevIconWindows" },
+          { (is_modified and "󰓏 " or ""), group = "DevIconCsv" },
+          { "┊ " },
+          { " " .. vim.api.nvim_win_get_number(props.win), group = "DevIconWindows" },
         }
       end,
     })
