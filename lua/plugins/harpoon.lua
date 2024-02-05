a113de57945bca04f1d393065b6ad7de3a3bfdfb
Ricardo Otero <oterosantos@gmail.com>
2024-02-05 23:26:18 +0000

    Add missing key bindings to harpoon

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -2,21 +2,38 @@ return {
   "ThePrimeagen/harpoon",
   branch = "harpoon2",
   dependencies = {
-    "nvim-lua/plenary.nvim"
+    "nvim-lua/plenary.nvim",
   },
   config = function()
     local harpoon = require("harpoon")
+
     vim.keymap.set("n", "<leader>a", function()
       harpoon:list():append()
     end, { desc = "Add file to Harpoon" })
+
     vim.keymap.set("n", "<leader>h", function()
       harpoon.ui:toggle_quick_menu(harpoon:list())
     end, { desc = "Toggle Harpoon menu" })
+
+    vim.keymap.set("n", "<C-h>", function()
+      harpoon:list():select(1)
+    end)
+    vim.keymap.set("n", "<C-t>", function()
+      harpoon:list():select(2)
+    end)
     vim.keymap.set("n", "<C-n>", function()
+      harpoon:list():select(3)
+    end)
+    vim.keymap.set("n", "<C-s>", function()
+      harpoon:list():select(4)
+    end)
+
+    vim.keymap.set("n", "<C-S-P>", function()
       harpoon:list():prev()
     end)
-    vim.keymap.set("n", "<C-m>", function()
+
+    vim.keymap.set("n", "<C-S-N>", function()
       harpoon:list():next()
     end)
-  end
+  end,
 }
