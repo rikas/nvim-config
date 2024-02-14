39fe1293dcb5be5b611ccb344e983aeb6056bf46
Ricardo Otero <oterosantos@gmail.com>
2024-02-14 15:53:37 +0000

    Delete the plugins_backup folder

--- plugins_backup/lualine.lua
+++
@@ -1,9 +0,0 @@
-require("lualine").setup({
-  extensions = { "fugitive", "lazy", "trouble", "neo-tree", "mason" },
-  options = {
-    theme = "auto",
-    icons_enabled = true,
-    component_separators = { left = "󰿟", right = "󰿟" },
-    section_separators = { left = "", right = "" },
-  },
-})
