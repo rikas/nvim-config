bcec7fef7f53b3235dd13ba8600fdd4779c5def6
Ricardo Otero <oterosantos@gmail.com>
2024-12-18 09:29:54 +0000

    Add comment

--- lua/rikas/keymaps.lua
+++ lua/rikas/keymaps.lua
@@ -28,6 +28,7 @@ vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 -- Q should do nothing!
 vim.keymap.set("n", "Q", "<nop>")
 
+-- Quickfix and location list navigation
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
