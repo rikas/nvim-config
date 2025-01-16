6c1016534ceefeb6f54229aaa7b05b41e7518036
Ricardo Otero <oterosantos@gmail.com>
2025-01-16 21:00:11 +0000

    Add mini.cursorword back

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,4 +1,5 @@
 _G.cursorword_blocklist = function()
+  local curword = vim.fn.expand("<cword>")
   local filetype = vim.bo.filetype
 
   -- Disable for neo-tree
@@ -6,6 +7,16 @@ _G.cursorword_blocklist = function()
     vim.b.minicursorword_disable = true
     return
   end
+
+  -- Add any disabling global or filetype-specific logic here
+  local blocklist = {}
+  if filetype == "lua" then
+    blocklist = { "local", "require" }
+  elseif filetype == "javascript" then
+    blocklist = { "import" }
+  end
+
+  vim.b.minicursorword_disable = vim.tbl_contains(blocklist, curword)
 end
 
 return { -- A set of small packages with common features
@@ -15,9 +26,9 @@ return { -- A set of small packages with common features
   config = function()
     require("mini.bufremove").setup()
     -- Show matching words like the one under the cursor
-    -- require("mini.cursorword").setup({
-    --   delay = 150,
-    -- })
+    require("mini.cursorword").setup({
+      delay = 150,
+    })
     require("mini.pairs").setup()
     require("mini.trailspace").setup()
     require("mini.splitjoin").setup()
