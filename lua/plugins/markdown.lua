cc09c3839b2884550c880cde45edb102a1649ea9
Ricardo Otero <oterosantos@gmail.com>
2025-08-19 15:02:15 +0100

    Live preview markdown

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,9 +1,17 @@
+-- return {
+--   enabled = false, -- this plugin was messing with markdown preview on function signatures etc
+--   "MeanderingProgrammer/markdown.nvim",
+--   name = "render-markdown", -- Only needed if you have another plugin named markdown.nvim
+--   dependencies = { "nvim-treesitter/nvim-treesitter" },
+--   config = function()
+--     require("render-markdown").setup({})
+--   end,
+-- }
+
 return {
-  enabled = false, -- this plugin was messing with markdown preview on function signatures etc
-  "MeanderingProgrammer/markdown.nvim",
-  name = "render-markdown", -- Only needed if you have another plugin named markdown.nvim
-  dependencies = { "nvim-treesitter/nvim-treesitter" },
-  config = function()
-    require("render-markdown").setup({})
-  end,
+  enabled = true,
+  "brianhuster/live-preview.nvim",
+  dependencies = {
+    "folke/snacks.nvim",
+  },
 }
