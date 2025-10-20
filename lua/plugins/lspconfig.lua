73156a391d1934a5531ca7e87306c5c8127d48eb
Ricardo Otero <oterosantos@gmail.com>
2025-10-21 00:10:18 +0100

    Remove unused plugin

--- lua/plugins/lspconfig.lua
+++
@@ -1,9 +0,0 @@
-return { -- LSP Configuration & Plugins
-  enable = false,
-  "neovim/nvim-lspconfig",
-  event = { "BufReadPost", "BufNewFile", "BufWritePre" },
-  dependencies = {
-    -- Useful status updates for LSP
-    "j-hui/fidget.nvim",
-  },
-}
