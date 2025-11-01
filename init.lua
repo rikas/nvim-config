a7beef477635b9bce2628027f4710a18d4e64e0d
Ricardo Otero <oterosantos@gmail.com>
2025-11-01 18:51:57 +0000

    Small tweak to avoid warning

--- init.lua
+++ init.lua
@@ -5,13 +5,15 @@ require("rikas.keymaps")
 require("rikas.options")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
+local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+
+if not vim.uv.fs_stat(lazypath) then
   vim.fn.system({
     "git",
     "clone",
     "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
     "--branch=stable", -- latest stable release
+    lazyrepo,
     lazypath,
   })
 end
