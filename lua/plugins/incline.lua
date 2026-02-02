0b2c543aa88af12ad467210505d653ef0fa56b62
Ricardo Otero <oterosantos@gmail.com>
2026-02-02 09:35:16 +0000

    Simplify incline even more

--- lua/plugins/incline.lua
+++ lua/plugins/incline.lua
@@ -1,13 +1,14 @@
 -- Name of the current buffer in the upper right corner
 return {
   "b0o/incline.nvim",
+  enabled = true,
   config = function()
     require("incline").setup({
       render = function(props)
-        local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
-        local ft_icon, ft_color = require("nvim-web-devicons").get_icon_color(filename)
+        -- local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
+        -- local ft_icon, ft_color = require("nvim-web-devicons").get_icon_color(filename)
         local is_modified = vim.bo[props.buf].modified
-        local modified = vim.bo[props.buf].modified and "bold,italic" or "none"
+        -- local modified = vim.bo[props.buf].modified and "bold,italic" or "none"
 
         local function get_diagnostic_label()
           local icons = { error = " ", warn = " ", info = " ", hint = " " }
@@ -30,11 +31,11 @@ return {
 
         return {
           { get_diagnostic_label() },
-          { (ft_icon or "") .. " ", guifg = ft_color, guibg = "none" },
-          { filename .. " ", gui = modified },
+          -- { (ft_icon or "") .. " ", guifg = ft_color, guibg = "none" },
+          -- { filename .. " ", gui = modified },
           { (is_modified and " " or ""), group = "DevIconCsv" },
-          { "┊ " },
-          { " " .. vim.api.nvim_win_get_number(props.win), group = "DevIconWindows" },
+          -- { "┊ " },
+          -- { " " .. vim.api.nvim_win_get_number(props.win), group = "DevIconWindows" },
         }
       end,
     })
