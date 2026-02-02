21d00756ef060726567fc7ca54777f35b31f6f22
Ricardo Otero <oterosantos@gmail.com>
2026-02-02 09:33:35 +0000

    Add prisma formatting

---
+++ lsp/prismals.lua
@@ -0,0 +1,14 @@
+---@type vim.lsp.Config
+return {
+  cmd = { "prisma-language-server", "--stdio" },
+  filetypes = { "prisma" },
+  settings = {
+    prisma = {
+      prismaFmtBinPath = "",
+    },
+  },
+  root_markers = {
+    ".git",
+    "package.json",
+  },
+}
